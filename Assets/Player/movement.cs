57ef12be849eef32df31299831ae8ef58db761a5
Markus <Markuspiotrowski@live.com>
2021-09-02 10:22:26 +0200

    ButtonContsollsTest

--- Assets/Player/movement.cs
+++ Assets/Player/movement.cs
@@ -9,21 +9,21 @@ public class Movement : MonoBehaviour
     float isGrounded;
     Rigidbody2D body;
 
-    float move; //-1 to 1
-    bool jump; //0 or 1
+    public float move; //-1 to 1
+    public bool jump; //0 or 1
     void awake()
     {
         body = GetComponent<Rigidbody2D>();
     }
     void Update()
     {
-        
+
     }
     void Jump()
     {
-        if(testGrounded() == true)
+        if (testGrounded() == true)
         {
-            
+
         }
     }
     bool testGrounded() //Tests if the player is grounded
@@ -31,6 +31,7 @@ public class Movement : MonoBehaviour
         RaycastHit2D castDown;
         castDown = Physics2D.Raycast(transform.position, Vector2.down, 1, LayerMask.NameToLayer("Ground"));
         Debug.DrawRay(transform.position, Vector2.down);
-        return(false);
+        return (false);
     }
+    
 }
